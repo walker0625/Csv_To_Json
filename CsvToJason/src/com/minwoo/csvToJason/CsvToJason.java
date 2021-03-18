956b98ce01d86cab04ac4ce6faa46f4afe8cb273
Minwoo Jeon <jeonminwoo0625@gmail.com>
2021-03-18 11:59:30 +0900

    calculateOneDayTradeDatas()에 method 및 javadoc 추가
    1. getPriceXvolume(), removeUnderZero() 메소드로 공통처리 묶음 및 가독성 향상
    2. javadoc으로 input/output을 명시하여 메소드에 대한 직관성 향상

--- CsvToJason/src/com/minwoo/csvToJason/CsvToJason.java
+++ CsvToJason/src/com/minwoo/csvToJason/CsvToJason.java
@@ -1,17 +1,17 @@
 package com.minwoo.csvToJason;
 
-import java.io.FileNotFoundException; 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.List;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.opencsv.CSVReader;
-import com.opencsv.exceptions.CsvValidationException;
 
 public class CsvToJason {
 
@@ -31,6 +31,10 @@ public class CsvToJason {
 		}
 	}
 
+	/**
+	 * @param csvDatas : 가공할 csv 내용들(시간, 가격, 갯수 형태)
+	 * @return List<OneDayTradeDataDto> : 1일(86400초)치씩 가공(계산)된 데이터를 담은 dto의 List
+	 */
 	public static List<OneDayTradeDataDto> separateOneDayData(CSVReader csvDatas) {
 		List<OneDayTradeDataDto> dtoList = new ArrayList<OneDayTradeDataDto>();
 		List<String[]> oneDayDatas = new ArrayList<String[]>();
@@ -72,18 +76,22 @@ public class CsvToJason {
 		return dtoList;
 	}
 
+	/**
+	 * @param oneDayDatas : 1일치(86400초)의 가공되지 않은 거래정보
+	 * @return OneDayTradeDataDto : 1일치(86400초)의 가공(계산)이 완료된 거래정보
+	 */
 	public static OneDayTradeDataDto calculateOneDayTradeDatas(List<String[]> oneDayDatas) {
 		int oneDayDatasSize = oneDayDatas.size();
 		int thisIndexPrice = 0;
 		int highestPrice = 0;
-		int lowestPrice = (int) Float.parseFloat(oneDayDatas.get(0)[1]);
+		int lowestPrice = Integer.parseInt(removeUnderZero(oneDayDatas.get(0)[1]));
 		int totalTradePrice = 0;
 		int totalTradeCount = 0;
 		BigDecimal totalWeightTradePrice = new BigDecimal(0.0);
 		BigDecimal volume = new BigDecimal(0.0);
 
 		for (int i = 0; i < oneDayDatasSize; i++) {
-			thisIndexPrice = (int) Float.parseFloat(oneDayDatas.get(i)[1]);
+			thisIndexPrice = Integer.parseInt(removeUnderZero(oneDayDatas.get(i)[1]));
 
 			if (highestPrice < thisIndexPrice) {
 				highestPrice = thisIndexPrice;
@@ -95,18 +103,19 @@ public class CsvToJason {
 
 			totalTradePrice += thisIndexPrice;
 			totalTradeCount++;
-			totalWeightTradePrice = totalWeightTradePrice
-					.add(new BigDecimal(oneDayDatas.get(i)[2]).multiply(new BigDecimal(thisIndexPrice)));
 
+			// oneDayDatas.get(i)[2]) : volume
+			totalWeightTradePrice = totalWeightTradePrice.add(getPriceXvolume(thisIndexPrice, oneDayDatas.get(i)[2]));
 			volume = volume.add(new BigDecimal(oneDayDatas.get(i)[2]));
 		}
 
 		OneDayTradeDataDto oneDayTradeDataDto = new OneDayTradeDataDto();
 
+		// [0] : time, [1] : price
 		oneDayTradeDataDto.setStart(Integer.parseInt(oneDayDatas.get(0)[0]));
 		oneDayTradeDataDto.setEnd(Integer.parseInt(oneDayDatas.get(oneDayDatasSize - 1)[0]));
-		oneDayTradeDataDto.setOpen(oneDayDatas.get(0)[1].substring(0, oneDayDatas.get(0)[1].indexOf(".")));
-		oneDayTradeDataDto.setClose(oneDayDatas.get(oneDayDatasSize - 1)[1].substring(0, oneDayDatas.get(oneDayDatasSize - 1)[1].indexOf(".")));
+		oneDayTradeDataDto.setOpen(removeUnderZero(oneDayDatas.get(0)[1]));
+		oneDayTradeDataDto.setClose(removeUnderZero(oneDayDatas.get(oneDayDatasSize - 1)[1]));
 		oneDayTradeDataDto.setHigh(Integer.toString(highestPrice));
 		oneDayTradeDataDto.setLow(Integer.toString(lowestPrice));
 		oneDayTradeDataDto.setAverage(Integer.toString(totalTradePrice / totalTradeCount));
@@ -116,6 +125,18 @@ public class CsvToJason {
 		return oneDayTradeDataDto;
 	}
 
+	public static BigDecimal getPriceXvolume(int price, String volume) {
+		return new BigDecimal(price).multiply(new BigDecimal(volume));
+	}
+
+	public static String removeUnderZero(String realNumber) {
+		return realNumber.substring(0, realNumber.indexOf("."));
+	}
+
+	/**
+	 * @param dtoList : 가공(계산)된 1일 거래정보들의 List
+	 * @return jsonArray(String[] 형태) : dtoList를 json의 형태로 변환한 [{}{}{}] 구조의 Data
+	 */
 	public static String[] convertJson(List<OneDayTradeDataDto> dtoList) {
 		ObjectMapper mapper = new ObjectMapper();
 		int dtoListSize = dtoList.size();
